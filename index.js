bd592c9f8544c7830164c283ea36b912eef952a3
Patrick Williams <patrick@stwcx.xyz>
2020-11-11 16:12:57 -0600

    add logging of various message events
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- index.js
+++ index.js
@@ -3,14 +3,36 @@ require('dotenv').config();
 var Discord = require('discord.js')
 var client = new Discord.Client();
 
-client.on('ready', () => {
+client.on(Discord.Constants.Events.CLIENT_READY, () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
+client.on(Discord.Constants.Events.MESSAGE_CREATE, msg => {
+    if ((msg.channel.type !== 'text') || (!msg.cleanContent)) {
+        return;
+    }
+    console.log(`Msg on ${msg.channel.name} from ${msg.author.username}: ` +
+        `${msg.cleanContent}`);
+
+    if (msg.cleanContent === 'ping') {
         msg.reply('pong');
     }
+});
+
+client.on(Discord.Constants.Events.MESSAGE_UPDATE, (oldMsg, msg) => {
+    if ((msg.channel.type !== 'text') || (!msg.cleanContent)) {
+        return;
+    }
+    console.log(`Msg on ${msg.channel.name} from ${msg.author.username} ` +
+        `edited: ${oldMsg.cleanContent} -> ${msg.cleanContent}`);
+});
+
+client.on(Discord.Constants.Events.MESSAGE_DELETE, msg => {
+    if ((msg.channel.type !== 'text') || (!msg.cleanContent)) {
+        return;
+    }
+    console.log(`Msg on ${msg.channel.name} from ${msg.author.username} ` +
+        `deleted: ${msg.cleanContent}`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
