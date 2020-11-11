de9d40983353b2410827133439746ea1f7b791be
Patrick Williams <patrick@stwcx.xyz>
2020-11-11 14:49:35 -0600

    hello-world level bot
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+var Discord = require('discord.js')
+var client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
